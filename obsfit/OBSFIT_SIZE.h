4a020561f3288ca66159c23e25563308adb9f1d4
averdy <44445771+averdy@users.noreply.github.com>
2022-07-20 09:58:14 -0700

    Add files via upload

--- obsfit/OBSFIT_SIZE.h
+++ obsfit/OBSFIT_SIZE.h
@@ -1,31 +1,31 @@
 #ifdef ALLOW_OBSFIT
 
 C ======================================================================
-C NFILESOBSMAX      :: maximum number of input files
-C NVARMAX           :: maximum number of variables 
-C NOBSGLOB          :: maximum number of observations per file and tile
-C NSAMPLEGLOB       :: maximum number of samples per file and tile
-C NPMAX             :: maximum number of samples per observation 
-C NUM_INTERP_POINTS :: number of points used in interpolation for 
-C                      model sampling
+C NFILESMAX_OF     :: maximum number of input files
+C NVARMAX_OF       :: maximum number of variables 
+C NOBSMAX_OF       :: maximum number of observations per file and tile
+C NSAMPLESMAX      :: maximum number of samples per file and tile
+C NSAMP_PER_OBS_MAX:: maximum number of samples per observation 
+C NUM_INTERP_PTS_OF:: number of points used in interpolation for 
+C                     model sampling
 C ======================================================================
 
-      INTEGER NFILESOBSMAX
-      PARAMETER ( NFILESOBSMAX=20 )
+      INTEGER NFILESMAX_OF
+      PARAMETER ( NFILESMAX_OF=20 )
 
-      INTEGER NVARMAX
-      PARAMETER ( NVARMAX=6 )
+      INTEGER NVARMAX_OF
+      PARAMETER ( NVARMAX_OF=6 )
 
-      INTEGER NOBSGLOB
-      PARAMETER ( NOBSGLOB=500 )
+      INTEGER NOBSMAX_OF
+      PARAMETER ( NOBSMAX_OF=500 )
 
-      INTEGER NSAMPLEGLOB
-      PARAMETER ( NSAMPLEGLOB=55000 )
+      INTEGER NSAMPLESMAX
+      PARAMETER ( NSAMPLESMAX=55000 )
 
-      INTEGER NPMAX
-      PARAMETER ( NPMAX=110 )
+      INTEGER NSAMP_PER_OBS_MAX
+      PARAMETER ( NSAMP_PER_OBS_MAX=110 )
 
-      INTEGER NUM_INTERP_POINTS
-      PARAMETER ( NUM_INTERP_POINTS=8 )
+      INTEGER NUM_INTERP_PTS_OF
+      PARAMETER ( NUM_INTERP_PTS_OF=8 )
 
 #endif /* ALLOW_OBSFIT */
--- obsfit/obsfit.h
+++ obsfit/obsfit.h
@@ -1,19 +1,18 @@
 C     ==================================================================
 
-      _RL sample_timeS(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy),
-     & sample_timeE(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy),
-     & sample_lon(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy),
-     & sample_lat(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy),
-     & sample_depth(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy),
-     & sample_weight(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy)
+      _RL sample_timeS(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy),
+     & sample_timeE(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy),
+     & sample_lon(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy),
+     & sample_lat(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy),
+     & sample_depth(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy),
+     & sample_weight(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy)
       _RL sample_modmask(nsx,nsy)
       _RL obs_modmask
-      _RL obs_delT(NFILESOBSMAX,NOBSGLOB)
+      _RL obs_delT(NFILESMAX_OF,NOBSMAX_OF)
 #ifndef ALLOW_ECCO
       _RL m_UE(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nsx,nsy)
       _RL m_VN(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nsx,nsy)
 #endif
-      _RL cs(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nsx,nsy)
       COMMON /obsfit_r/ sample_timeS, sample_timeE,
      & sample_lon, sample_lat,
      & sample_depth, sample_weight, sample_modmask,
@@ -21,29 +20,28 @@ C     ==================================================================
 #ifndef ALLOW_ECCO
      & ,m_UE, m_VN
 #endif /* ALLOW_ECCO */
-     & ,cs
 
-      integer obs_ind_glob(NFILESOBSMAX,NOBSGLOB)
-      integer obs_np(NFILESOBSMAX,NOBSGLOB)
-      integer obsfitOperation(NFILESOBSMAX)
-      integer sample_ind_glob(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy)
-      integer ObsNo(NFILESOBSMAX)
-      integer sampleNo(NFILESOBSMAX,nsx,nsy)
-      integer fidforward(NFILESOBSMAX,nsx,nsy), 
-     & fidadjoint(NFILESOBSMAX,nsx,nsy), 
-     & fidtangent(NFILESOBSMAX,nsx,nsy)
-      integer fiddata(NFILESOBSMAX)
-      integer fidglobal(NFILESOBSMAX)
-      integer fidadglobal(NFILESOBSMAX)
-      integer fidtangglobal(NFILESOBSMAX)
-      integer obs_num_var_tot(NFILESOBSMAX)
-      integer obs_num_var_cur(NFILESOBSMAX,NVARMAX)
-      integer obsfit_itracer(NFILESOBSMAX,NVARMAX)
-      integer obs_sample1_ind(NFILESOBSMAX,NOBSGLOB)
+      integer obs_ind_glob(NFILESMAX_OF,NOBSMAX_OF)
+      integer obs_np(NFILESMAX_OF,NOBSMAX_OF)
+      integer obsfitOperation(NFILESMAX_OF)
+      integer sample_ind_glob(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy)
+      integer ObsNo(NFILESMAX_OF)
+      integer sampleNo(NFILESMAX_OF,nsx,nsy)
+      integer fidfwd_of(NFILESMAX_OF,nsx,nsy), 
+     & fidadj_of(NFILESMAX_OF,nsx,nsy), 
+     & fidtan_of(NFILESMAX_OF,nsx,nsy)
+      integer fiddata(NFILESMAX_OF)
+      integer fidglobal(NFILESMAX_OF)
+      integer fidadglobal(NFILESMAX_OF)
+      integer fidtangglobal(NFILESMAX_OF)
+      integer obs_num_var_tot(NFILESMAX_OF)
+      integer obs_num_var_cur(NFILESMAX_OF,NVARMAX_OF)
+      integer obsfit_itracer(NFILESMAX_OF,NVARMAX_OF)
+      integer obs_sample1_ind(NFILESMAX_OF,NOBSMAX_OF)
       COMMON /obsfit_i/ obs_ind_glob, obs_np, 
      & obsfitOperation,
      & sample_ind_glob, ObsNo, sampleNo,
-     & fidforward, fidadjoint, fidtangent, fiddata, 
+     & fidfwd_of, fidadj_of, fidtan_of, fiddata, 
      & fidglobal, fidadglobal, fidtangglobal,
      & obs_num_var_tot, obs_num_var_cur, obsfit_itracer,
      & obs_sample1_ind
@@ -51,19 +49,19 @@ C     ==================================================================
 
 C-  Grid
 c( generic grid
-      _RL sample_interp_xC11(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy)
-      _RL sample_interp_yC11(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy)
-      _RL sample_interp_xCNINJ(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy)
-      _RL sample_interp_yCNINJ(NFILESOBSMAX,NSAMPLEGLOB,nsx,nsy)
+      _RL sample_interp_xC11(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy)
+      _RL sample_interp_yC11(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy)
+      _RL sample_interp_xCNINJ(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy)
+      _RL sample_interp_yCNINJ(NFILESMAX_OF,NSAMPLESMAX,nsx,nsy)
 c)
-      _RL sample_interp_w(NFILESOBSMAX,NSAMPLEGLOB,
-     &     NUM_INTERP_POINTS,nsx,nsy)
-      integer sample_interp_i(NFILESOBSMAX,NSAMPLEGLOB,
-     &     NUM_INTERP_POINTS,nsx,nsy)
-      integer sample_interp_j(NFILESOBSMAX,NSAMPLEGLOB,
-     &     NUM_INTERP_POINTS,nsx,nsy)
-      integer sample_interp_k(NFILESOBSMAX,NSAMPLEGLOB,
-     &     NUM_INTERP_POINTS,nsx,nsy)
+      _RL sample_interp_w(NFILESMAX_OF,NSAMPLESMAX,
+     &     NUM_INTERP_PTS_OF,nsx,nsy)
+      integer sample_interp_i(NFILESMAX_OF,NSAMPLESMAX,
+     &     NUM_INTERP_PTS_OF,nsx,nsy)
+      integer sample_interp_j(NFILESMAX_OF,NSAMPLESMAX,
+     &     NUM_INTERP_PTS_OF,nsx,nsy)
+      integer sample_interp_k(NFILESMAX_OF,NSAMPLESMAX,
+     &     NUM_INTERP_PTS_OF,nsx,nsy)
       COMMON /obsfit_grid_r/ sample_interp_w,
      & sample_interp_xC11, sample_interp_yC11,
      & sample_interp_xCNINJ, sample_interp_yCNINJ  
@@ -72,22 +70,22 @@ c)
 
 
 C-  Logical
-      logical var_in_obsfile(NFILESOBSMAX,NVARMAX)
+      logical var_in_obsfile(NFILESMAX_OF,NVARMAX_OF)
       logical obsfitDoNcOutput, obsfitDoGenGrid
       COMMON /obsfit_l/ var_in_obsfile, obsfitDoNcOutput, 
      & obsfitDoGenGrid
 C-  Characters
-      character*(8) obsfit_names(NFILESOBSMAX,NVARMAX)
-      character*(12) obsfit_namesmask(NFILESOBSMAX,NVARMAX)
-      character*(14) obsfit_namesweight(NFILESOBSMAX,NVARMAX)
-      character*(8) obsfit_namesmod(NFILESOBSMAX,NVARMAX)
+      character*(8) obsfit_names(NFILESMAX_OF,NVARMAX_OF)
+      character*(12) obsfit_namesmask(NFILESMAX_OF,NVARMAX_OF)
+      character*(14) obsfit_namesweight(NFILESMAX_OF,NVARMAX_OF)
+      character*(8) obsfit_namesmod(NFILESMAX_OF,NVARMAX_OF)
       COMMON /obsfit_c/ obsfit_names, obsfit_namesmask,
      & obsfit_namesweight, obsfit_namesmod
 
 
 C-  Buffers
-      _RL obsfit_data_buff(1000,NVARMAX)
-      _RL obsfit_weight_buff(1000,NVARMAX)
+      _RL obsfit_data_buff(1000,NVARMAX_OF)
+      _RL obsfit_weight_buff(1000,NVARMAX_OF)
       integer obsfit_minind_buff
       integer obsfit_maxind_buff
       integer obsfit_curfile_buff
@@ -97,10 +95,10 @@ C-  Buffers
 
 
 C-  Cost
-      _RL  objf_obsfit(NFILESOBSMAX,NVARMAX)
-      _RL  num_obsfit(NFILESOBSMAX,NVARMAX)
-      _RL  mult_obsfit(NFILESOBSMAX,NVARMAX)
-      _RL  obsfit_facmod(NFILESOBSMAX,NVARMAX)
+      _RL  objf_obsfit(NFILESMAX_OF,NVARMAX_OF)
+      _RL  num_obsfit(NFILESMAX_OF,NVARMAX_OF)
+      _RL  mult_obsfit(NFILESMAX_OF,NVARMAX_OF)
+      _RL  obsfit_facmod(NFILESMAX_OF,NVARMAX_OF)
       COMMON /obsfit_cost_r/
      &                objf_obsfit,
      &                num_obsfit,
@@ -108,12 +106,12 @@ C-  Cost
      &                obsfit_facmod
      
       character*(MAX_LEN_FNAM) obsfitDir
-      character*(MAX_LEN_FNAM) obsfitFiles(NFILESOBSMAX)
+      character*(MAX_LEN_FNAM) obsfitFiles(NFILESMAX_OF)
       COMMON /obsfit_cost_c/
      &        obsfitDir, obsfitFiles
      
-      _RL obsfit_dummy(NFILESOBSMAX,NVARMAX,nsx,nsy)
-      _RL obsfit_globaldummy(NFILESOBSMAX,NVARMAX)
+      _RL obsfit_dummy(NFILESMAX_OF,NVARMAX_OF,nsx,nsy)
+      _RL obsfit_globaldummy(NFILESMAX_OF,NVARMAX_OF)
       COMMON /obsfit_ctrl_dummy/
      &                obsfit_dummy,
      &                obsfit_globaldummy
