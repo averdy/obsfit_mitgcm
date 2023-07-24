f511515af0c9ce109f2363a1d962afa99196c7f8
averdy <44445771+averdy@users.noreply.github.com>
2023-07-24 14:34:39 -0700

    Add files via upload

--- global_oce_biogeo_bling/code_ad_obsfit/OBSFIT_SIZE.h
+++ global_oce_biogeo_bling/code_ad_obsfit/OBSFIT_SIZE.h
@@ -1,29 +1,31 @@
 #ifdef ALLOW_OBSFIT
 
 C ======================================================================
-C NFILESMAX_OBS     :: maximum number of input files
-C NVARMAX_OBS       :: maximum number of variables 
-C NOBSMAX_OBS       :: maximum number of observations per file and tile
-C NSAMPLESMAX       :: maximum number of samples per file and tile
-C NSAMP_PER_OBS_MAX :: maximum number of samples per observation 
-C NUM_INTERP_PTS_OBS:: number of points used in interpolation for 
-C                      model sampling
+C NFILESMAX_OBS      :: maximum number of input files
+C NVARMAX_OBS        :: maximum number of variables 
+C NOBSMAX_OBS        :: maximum number of observations per file and tile
+C NSAMPLESMAX        :: maximum number of samples per file and tile
+C NSAMP_PER_OBS_MAX  :: maximum number of samples per observation 
+C NUM_INTERP_PTS_OBS :: number of points used in interpolation for 
+C                       model sampling
 C ======================================================================
 
       INTEGER NFILESMAX_OBS
-      PARAMETER ( NFILESMAX_OBS=20 )
+      PARAMETER ( NFILESMAX_OBS=1 )
 
+CAV Currently the obsfit package only accepts 1 type of obs. 
+CAV Keeping this in for now...
       INTEGER NVARMAX_OBS
-      PARAMETER ( NVARMAX_OBS=6 )
+      PARAMETER ( NVARMAX_OBS=1 )
 
       INTEGER NOBSMAX_OBS
-      PARAMETER ( NOBSMAX_OBS=10000 )
+      PARAMETER ( NOBSMAX_OBS=55000 )
 
       INTEGER NSAMPLESMAX
       PARAMETER ( NSAMPLESMAX=55000 )
 
       INTEGER NSAMP_PER_OBS_MAX
-      PARAMETER ( NSAMP_PER_OBS_MAX=1000 )
+      PARAMETER ( NSAMP_PER_OBS_MAX=10 )
 
       INTEGER NUM_INTERP_PTS_OBS
       PARAMETER ( NUM_INTERP_PTS_OBS=8 )
