76e518dbe2ec5e48a52cf52eefaf5fbc4507d34f
averdy <44445771+averdy@users.noreply.github.com>
2024-04-22 12:18:23 -0700

    Add files via upload

--- obsfit/OBSFIT_SIZE.h
+++ obsfit/OBSFIT_SIZE.h
@@ -1,22 +1,30 @@
-#ifdef ALLOW_OBSFIT
+CBOP
+C     !ROUTINE: OBSFIT_SIZE.h
+C     !INTERFACE:
+C     #include "OBSFIT_SIZE.h"
 
-C ======================================================================
-C NFILESMAX_OBS    :: maximum number of input files
-C NOBSMAX_OBS      :: maximum number of observations per file and tile
-C NSAMPLESMAX      :: maximum number of samples per file and tile
-C NSAMP_PER_OBS_MAX:: maximum number of samples per observation 
-C NUM_INTERP_PTS_OBS:: number of points used in interpolation for 
-C                     model sampling
-C ======================================================================
+C     !DESCRIPTION:
+C     *================================================================*
+C     | OBSFIT_SIZE.h
+C     | o Header file defining "obsfit" arrays sizes
+C     *================================================================*
+CEOP
+
+C NFILESMAX_OBS      :: maximum number of input files
+C NOBSMAX_OBS        :: maximum number of observations per file per tile
+C NSAMPLESMAX        :: maximum number of samples per file per tile
+C NSAMP_PER_OBS_MAX  :: maximum number of samples per observation 
+C NUM_INTERP_PTS_OBS :: number of points used in interpolation for 
+C                       model sampling
 
       INTEGER NFILESMAX_OBS
       PARAMETER ( NFILESMAX_OBS=5 )
 
       INTEGER NOBSMAX_OBS
-      PARAMETER ( NOBSMAX_OBS=550000 )
+      PARAMETER ( NOBSMAX_OBS=10000 )
 
       INTEGER NSAMPLESMAX
-      PARAMETER ( NSAMPLESMAX=550000 )
+      PARAMETER ( NSAMPLESMAX=10000 )
 
       INTEGER NSAMP_PER_OBS_MAX
       PARAMETER ( NSAMP_PER_OBS_MAX=1 )
@@ -24,4 +32,3 @@ C ======================================================================
       INTEGER NUM_INTERP_PTS_OBS
       PARAMETER ( NUM_INTERP_PTS_OBS=8 )
 
-#endif /* ALLOW_OBSFIT */
