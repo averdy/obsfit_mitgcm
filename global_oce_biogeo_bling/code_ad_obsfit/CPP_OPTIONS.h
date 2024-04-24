da26e9822d0a0d80dbe5d8aa020992b522d4b261
averdy <44445771+averdy@users.noreply.github.com>
2024-04-24 09:30:02 -0700

    Add files via upload

--- global_oce_biogeo_bling/code_ad_obsfit/CPP_OPTIONS.h
+++ global_oce_biogeo_bling/code_ad_obsfit/CPP_OPTIONS.h
@@ -15,49 +15,15 @@ CEOP
 
 C CPP flags controlling particular source code features
 
-C-- Forcing code options:
-
 C o Shortwave heating as extra term in external_forcing.F
 C Note: this should be a run-time option
-#undef SHORTWAVE_HEATING
+#define SHORTWAVE_HEATING
 
 C o Include/exclude Geothermal Heat Flux at the bottom of the ocean
 #undef ALLOW_GEOTHERMAL_FLUX
 
-C o Allow to account for heating due to friction (and momentum dissipation)
-#undef ALLOW_FRICTION_HEATING
-
-C o Allow mass source or sink of Fluid in the interior
-C   (3-D generalisation of oceanic real-fresh water flux)
-#undef ALLOW_ADDFLUID
-
-C o Include pressure loading code
-#define ATMOSPHERIC_LOADING
-
-C o Include/exclude balancing surface forcing fluxes code
-#undef ALLOW_BALANCE_FLUXES
-
-C o Include/exclude balancing surface forcing relaxation code
-#undef ALLOW_BALANCE_RELAX
-
-C o Include/exclude checking for negative salinity
-#undef CHECK_SALINITY_FOR_NEGATIVE_VALUES
-
-C-- Options to discard parts of the main code:
-
-C o Exclude/allow external forcing-fields load
-C   this allows to read & do simple linear time interpolation of oceanic
-C   forcing fields, if no specific pkg (e.g., EXF) is used to compute them.
-#undef EXCLUDE_FFIELDS_LOAD
-
 C o Include/exclude phi_hyd calculation code
 #define INCLUDE_PHIHYD_CALCULATION_CODE
-
-C o Include/exclude sound speed calculation code
-C o (Note that this is a diagnostic from Del Grasso algorithm, not derived from EOS)
-#define INCLUDE_SOUNDSPEED_CALC_CODE
-
-C-- Vertical mixing code options:
 
 C o Include/exclude call to S/R CONVECT
 #define INCLUDE_CONVECT_CALL
@@ -71,61 +37,57 @@ C o Allow full 3D specification of vertical diffusivity
 C o Allow latitudinally varying BryanLewis79 vertical diffusivity
 #undef ALLOW_BL79_LAT_VARY
 
-C o Exclude/allow partial-cell effect (physical or enhanced) in vertical mixing
-C   this allows to account for partial-cell in vertical viscosity and diffusion,
-C   either from grid-spacing reduction effect or as artificially enhanced mixing
-C   near surface & bottom for too thin grid-cell
-#undef EXCLUDE_PCELL_MIX_CODE
-
-C o Exclude/allow to use isotropic 3-D Smagorinsky viscosity as diffusivity
-C   for tracers (after scaling by constant Prandtl number)
-#undef ALLOW_SMAG_3D_DIFFUSIVITY
-
-C-- Time-stepping code options:
-
-C o Include/exclude combined Surf.Pressure and Drag Implicit solver code
-#undef ALLOW_SOLVE4_PS_AND_DRAG
-
 C o Include/exclude Implicit vertical advection code
 #define INCLUDE_IMPLVERTADV_CODE
 
 C o Include/exclude AdamsBashforth-3rd-Order code
-#undef ALLOW_ADAMSBASHFORTH_3
-
-C o Include/exclude Quasi-Hydrostatic Stagger Time-step AdamsBashforth code
-#undef ALLOW_QHYD_STAGGER_TS
-
-C-- Model formulation options:
-
-C o Allow/exclude "Exact Convervation" of fluid in Free-Surface formulation
-C   that ensures that d/dt(eta) is exactly equal to - Div.Transport
-#define EXACT_CONSERV
-
-C o Allow the use of Non-Linear Free-Surface formulation
-C   this implies that grid-cell thickness (hFactors) varies with time
-#undef NONLIN_FRSURF
+#define ALLOW_ADAMSBASHFORTH_3
 
 C o Include/exclude nonHydrostatic code
 #undef ALLOW_NONHYDROSTATIC
 
+C o Allow to account for heating due to friction (and momentum dissipation)
+#undef ALLOW_FRICTION_HEATING
+
+C o Allow mass source or sink of Fluid in the interior
+C   (3-D generalisation of oceanic real-fresh water flux)
+#undef ALLOW_ADDFLUID
+
+C o Include pressure loading code
+#define ATMOSPHERIC_LOADING
+
+C o exclude/allow external forcing-fields load
+C   this allows to read & do simple linear time interpolation of oceanic
+C   forcing fields, if no specific pkg (e.g., EXF) is used to compute them.
+#undef EXCLUDE_FFIELDS_LOAD
+
+C o Include/exclude balancing surface forcing fluxes code
+#undef ALLOW_BALANCE_FLUXES
+
+C o Include/exclude balancing surface forcing relaxation code
+#undef ALLOW_BALANCE_RELAX
+
 C o Include/exclude GM-like eddy stress in momentum code
 #undef ALLOW_EDDYPSI
 
-C-- Algorithm options:
+C o Use "Exact Convervation" of fluid in Free-Surface formulation
+C   so that d/dt(eta) is exactly equal to - Div.Transport
+#undef EXACT_CONSERV
 
-C o Use Non Self-Adjoint (NSA) conjugate-gradient solver
-#undef ALLOW_CG2D_NSA
+C o Allow the use of Non-Linear Free-Surface formulation
+C   this implies that surface thickness (hFactors) vary with time
+#undef NONLIN_FRSURF
+#undef DISABLE_RSTAR_CODE
+#define DISABLE_SIGMA_CODE
 
 C o Include/exclude code for single reduction Conjugate-Gradient solver
-#define ALLOW_SRCG
+#undef ALLOW_SRCG
 
 C o Choices for implicit solver routines solve_*diagonal.F
 C   The following has low memory footprint, but not suitable for AD
 #undef SOLVE_DIAGONAL_LOWMEMORY
 C   The following one suitable for AD but does not vectorize
-#undef SOLVE_DIAGONAL_KINNER
-
-C-- Retired code options:
+#define SOLVE_DIAGONAL_KINNER
 
 C o ALLOW isotropic scaling of harmonic and bi-harmonic terms when
 C   using an locally isotropic spherical grid with (dlambda) x (dphi*cos(phi))
@@ -150,24 +112,23 @@ C   The definition of the flag is commented to avoid interference with
 C   such other header files.
 C#define COSINEMETH_III
 
+C o Use "OLD" UV discretisation near boundaries (*not* recommended)
+C   Note - only works with pkg/mom_fluxform and "no_slip_sides=.FALSE."
+C          because the old code did not have no-slip BCs
+#undef OLD_ADV_BCS
+
 C o Use LONG.bin, LATG.bin, etc., initialization for ini_curviliear_grid.F
 C   Default is to use "new" grid files (OLD_GRID_IO undef) but OLD_GRID_IO
 C   is still useful with, e.g., single-domain curvilinear configurations.
 #undef OLD_GRID_IO
 
-C-- Other option files:
+C o Use old EXTERNAL_FORCING_U,V,T,S subroutines (for backward compatibility)
+#undef USE_OLD_EXTERNAL_FORCING
+
+#define EXCLUDE_PCELL_MIX_CODE
 
 C o Execution environment support options
 #include "CPP_EEOPTIONS.h"
 
-C o Include/exclude single header file containing multiple packages options
-C   (AUTODIFF, COST, CTRL, ECCO, EXF ...) instead of the standard way where
-C   each of the above pkg get its own options from its specific option file.
-C   Although this method, inherited from ECCO setup, has been traditionally
-C   used for all adjoint built, work is in progress to allow to use the
-C   standard method also for adjoint built.
-c#ifdef PACKAGES_CONFIG_H
-c# include "ECCO_CPPOPTIONS.h"
-c#endif
-
 #endif /* CPP_OPTIONS_H */
+
--- global_oce_biogeo_bling/code_ad_obsfit/OBSFIT_SIZE.h
+++ global_oce_biogeo_bling/code_ad_obsfit/OBSFIT_SIZE.h
@@ -1,28 +1,34 @@
-#ifdef ALLOW_OBSFIT
+CBOP
+C     !ROUTINE: OBSFIT_SIZE.h
+C     !INTERFACE:
+C     #include "OBSFIT_SIZE.h"
 
-C ======================================================================
+C     !DESCRIPTION:
+C     *================================================================*
+C     | OBSFIT_SIZE.h
+C     | o Header file defining "obsfit" arrays sizes
+C     *================================================================*
+CEOP
+
 C NFILESMAX_OBS      :: maximum number of input files
-C NVARMAX_OBS        :: maximum number of variables 
-C NOBSMAX_OBS        :: maximum number of observations per file and tile
-C NSAMPLESMAX        :: maximum number of samples per file and tile
+C NOBSMAX_OBS        :: maximum number of observations per file per tile
+C NSAMPLESMAX        :: maximum number of samples per file per tile
 C NSAMP_PER_OBS_MAX  :: maximum number of samples per observation 
 C NUM_INTERP_PTS_OBS :: number of points used in interpolation for 
 C                       model sampling
-C ======================================================================
 
       INTEGER NFILESMAX_OBS
       PARAMETER ( NFILESMAX_OBS=5 )
 
       INTEGER NOBSMAX_OBS
-      PARAMETER ( NOBSMAX_OBS=55000 )
+      PARAMETER ( NOBSMAX_OBS=11000 )
 
       INTEGER NSAMPLESMAX
-      PARAMETER ( NSAMPLESMAX=55000 )
+      PARAMETER ( NSAMPLESMAX=11000 )
 
       INTEGER NSAMP_PER_OBS_MAX
-      PARAMETER ( NSAMP_PER_OBS_MAX=1 )
+      PARAMETER ( NSAMP_PER_OBS_MAX=2 )
 
       INTEGER NUM_INTERP_PTS_OBS
       PARAMETER ( NUM_INTERP_PTS_OBS=8 )
 
-#endif /* ALLOW_OBSFIT */
