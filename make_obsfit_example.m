d27ddb4efe0ed26b0a9da66b7e78bd9615a9fea0
averdy <44445771+averdy@users.noreply.github.com>
2024-10-11 11:12:31 -0700

    Update make_obsfit_example.m

--- make_obsfit_example.m
+++ make_obsfit_example.m
@@ -31,7 +31,7 @@ sample_weight = repmat(1,[1 n_obs]);
 
 % Location of samples
 sample_lon = linspace(220,230,n_obs);
-sample_lat = 0*sample_x;
+sample_lat = 0*sample_lon;
 sample_depth = repmat(0,[1 n_obs]);
 
 
@@ -39,7 +39,7 @@ sample_depth = repmat(0,[1 n_obs]);
 ncid = netcdf.create('obsfit_ssh.nc','NC_CLOBBER');
 
 % define dimensions
-sampledim_id = netcdf.defDim(ncid,'iSAMPLE',length(sample_x));
+sampledim_id = netcdf.defDim(ncid,'iSAMPLE',length(sample_lon));
 obsdim_id = netcdf.defDim(ncid,'iOBS',length(obs_val));
 
 % define variables
@@ -47,9 +47,9 @@ datevar_id = netcdf.defVar(ncid,'obs_date','double',obsdim_id);
 obsymd_id = netcdf.defVar(ncid,'obs_YYYYMMDD','double',obsdim_id);
 obshms_id = netcdf.defVar(ncid,'obs_HHMMSS','double',obsdim_id);
 obsnp_id = netcdf.defVar(ncid,'obs_np','double',obsdim_id);
-samplelon_id = netcdf.defVar(ncid,'sample_x','double',sampledim_id);
-samplelat_id = netcdf.defVar(ncid,'sample_y','double',sampledim_id);
-sampledepth_id = netcdf.defVar(ncid,'sample_z','double',sampledim_id);
+samplelon_id = netcdf.defVar(ncid,'sample_lon','double',sampledim_id);
+samplelat_id = netcdf.defVar(ncid,'sample_lat','double',sampledim_id);
+sampledepth_id = netcdf.defVar(ncid,'sample_depth','double',sampledim_id);
 sampletype_id = netcdf.defVar(ncid,'sample_type','double',sampledim_id);
 sampleweight_id = netcdf.defVar(ncid,'sample_weight','double',sampledim_id);
 obsval_id = netcdf.defVar(ncid,'obs_val','double',[obsdim_id]); 
@@ -62,9 +62,9 @@ netcdf.endDef(ncid);
 netcdf.putVar(ncid,obsymd_id,obs_YYYYMMDD);
 netcdf.putVar(ncid,obshms_id,obs_HHMMSS);
 netcdf.putVar(ncid,obsnp_id,obs_np);
-netcdf.putVar(ncid,samplelon_id,sample_x);
-netcdf.putVar(ncid,samplelat_id,sample_y);
-netcdf.putVar(ncid,sampledepth_id,sample_z);
+netcdf.putVar(ncid,samplelon_id,sample_lon);
+netcdf.putVar(ncid,samplelat_id,sample_lat);
+netcdf.putVar(ncid,sampledepth_id,sample_depth);
 netcdf.putVar(ncid,sampletype_id,sample_type);
 netcdf.putVar(ncid,sampleweight_id,sample_weight);
 netcdf.putVar(ncid,obsval_id,obs_val);
